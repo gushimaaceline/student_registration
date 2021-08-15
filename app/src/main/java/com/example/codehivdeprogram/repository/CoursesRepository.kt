7b34854b3a371cf964b52938351243b8a722496b
Celine Gushima <79209691+gushimaaceline@users.noreply.github.com>
2021-08-15 23:37:09 +0300

    Update CoursesRepository.kt

--- app/src/main/java/com/example/codehivdeprogram/repository/CoursesRepository.kt
+++ app/src/main/java/com/example/codehivdeprogram/repository/CoursesRepository.kt
@@ -1,4 +1,4 @@
-package com.example.registration.repository
+package com.example.codehivdeprogram.repository
 
 import com.example.registration.api.ApiClient
 import com.example.registration.api.ApiInterface
