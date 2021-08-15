ad52d3b41cf256f0484defd9c08778cc71c089ee
Celine Gushima <79209691+gushimaaceline@users.noreply.github.com>
2021-08-15 23:40:00 +0300

    Update UserViewModel.kt

--- app/src/main/java/com/example/codehivdeprogram/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/codehivdeprogram/viewmodel/UserViewModel.kt
@@ -3,28 +3,25 @@ package com.example.codehivdeprogram.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.codehivdeprogram.models.RegistrationRequest
-import com.example.codehivdeprogram.models.RegistrationResponse
-import com.example.codehivdeprogram.repository.UserRepository
+import com.example.registration.models.RegistrationRequest
+import com.example.registration.models.RegistrationResponse
+import com.example.registration.repository.UserRepository
 import kotlinx.coroutines.launch
 
-class UserViewModel:ViewModel() {
-    var registrationLiveData= MutableLiveData<RegistrationResponse>()
-    var regFailedLiveData= MutableLiveData<String>()
-    var userRepository= UserRepository()
+class UserViewModel: ViewModel() {
+    var registrationLiveData = MutableLiveData<RegistrationResponse>()
+    var regFailedLiveData = MutableLiveData<String>()
+    var userRepository = UserRepository()
 
-    fun registerStudent(registrationRequest:RegistrationRequest:RegistrationRequest){
-        viewModelScope.launch{
-          var response= userRepository.registerStudent(registrationRequest){
-              if (response.isSuccessful){
-                  registrationRequest.postValue(response.body())
-              }
-              else{
-                  regFailedLiveData.postValue(response.errorBody()?.string())
-              }
-          }
+    fun registerStudent(registrationRequest: RegistrationRequest){
+        viewModelScope.launch {
+            var response = userRepository.registerStudent(registrationRequest)
+            if (response.isSuccessful){
+                registrationLiveData.postValue(response.body())
+            }
+            else{
+                regFailedLiveData.postValue(response.errorBody()?.string())
+            }
         }
     }
-
-
 }
