82615ab991c13ace69b916dcd93341b7082dddac
Celine Gushima <79209691+gushimaaceline@users.noreply.github.com>
2021-08-15 23:00:37 +0300

    Update RegistrationRequest.kt

--- app/src/main/java/com/example/codehivdeprogram/models/RegistrationRequest.kt
+++ app/src/main/java/com/example/codehivdeprogram/models/RegistrationRequest.kt
@@ -1,12 +1,12 @@
-package com.example.codehivdeprogram.models
+package com.example.registration.models
 
 import com.google.gson.annotations.SerializedName
 
 data class RegistrationRequest(
-    var name:String,
+    var name: String,
     @SerializedName("phone_number") var phoneNumber: String,
-    var email:String,
-    @SerializedName("date_of_birth") var dateOfBirth:String,
-    var nationality: String,
-    var password:String,
+    var email: String,
+    @SerializedName("date_of_birth") var dateOfBirth: String,
+    var password: String,
+    var nationality: String
 )
